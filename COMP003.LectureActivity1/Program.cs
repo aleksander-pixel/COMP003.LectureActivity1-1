9da2bad43f1142131deac95c7c5674a506e678ca
Aleke <Aleke@LAPTOP-F4S9HMA7>
2025-01-16 14:50:19 -0800

    Add project files.

---
+++ COMP003.LectureActivity1/Program.cs
@@ -0,0 +1,11 @@
+﻿// this name space contain all the codes for the application
+namespace COMP003.LectureActivity1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
